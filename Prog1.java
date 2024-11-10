7fc836041291411fee2b9fb8ed0e0658dfb7815b
HrishikeshKudtarkar <hrishikesh.kudtarkar@svisnavimumbai.edu.in>
2024-11-10 19:22:53 +0530

    Create Prog1.java

---
+++ Prog1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class pg185_d19_9__prog1
+{
+    public static void main()
+    {
+        Scanner sc = new Scanner(System.in);
+        double n= 10;
+        double i= 1;
+        for(i=1;i<=n;i++)
+        {
+            System.out.println("Happy New Year");
+        }
+    }
+}
