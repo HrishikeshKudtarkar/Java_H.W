3baebe00343c6b4007934c38f5842edd35809350
HrishikeshKudtarkar <hrishikesh.kudtarkar@svisnavimumbai.edu.in>
2024-11-10 22:30:48 +0530

    Create Ch-8_Prog2.java

---
+++ Ch-8_Prog2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class pg187_d19_9__prog2
+{
+    public static void main()
+    {
+        Scanner sc = new Scanner(System.in);
+        int n= 10;
+        int i= 1;
+        for(i=1;i<=10;i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
