8f07c20b7398d45a8f5362d53ddd842376ea7fbb
HrishikeshKudtarkar <hrishikesh.kudtarkar@svisnavimumbai.edu.in>
2024-11-12 22:01:00 +0530

    Create 11.Ch-8_Pg201.java

---
+++ 11.Ch-8_Pg201.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class pg201_Prog11
+{
+    public static void main()
+    {
+        int i=0;
+        while(i<=9)
+        {
+            i++;
+            if(i%2==0)
+            continue;
+            System.out.println(i);
+        }
+    }
+}
