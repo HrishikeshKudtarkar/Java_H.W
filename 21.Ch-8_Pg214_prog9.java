d2d2110393f8598d4dc1a7bd0bcea800b5ef6f13
HrishikeshKudtarkar <hrishikesh.kudtarkar@svisnavimumbai.edu.in>
2024-11-23 23:20:17 +0530

    Create 21.Ch-8_Pg214_prog9.java

---
+++ 21.Ch-8_Pg214_prog9.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class pg214_Prog9
+{
+    public static void main ()
+    {
+        Scanner sc= new Scanner(System.in);
+        int a,m,n,min,max;
+        System.out.println("Enter the first number");
+        n=sc.nextInt();
+        max=n; min=n;
+        System.out.println("Enter the remaining 9 numbers one by one.");
+        for(a=2;a<=10;a++)
+        {
+            m=sc.nextInt();
+            if(m<min)
+            min=m;
+            if(m>max)
+            max=m;
+        }
+        System.out.println("Largest number is "+max);
+        System.out.println("Smallest number is "+min);
+    }
+}
